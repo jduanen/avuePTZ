36eb256d122ccf1b7e919d560dff496cafe1fbd0
Duane Northcutt <duane.northcutt@gmail.com>
2021-12-01 14:49:09 -0800

    Stubbed out mqtt.

--- avueLogger.py
+++ avueLogger.py
@@ -7,6 +7,7 @@ import argparse
 import json
 import logging
 import os
+import re
 import signal
 import sys
 from threading import Timer
@@ -14,13 +15,85 @@ import time
 import yaml
 from yaml import Loader
 
+#import paho.mqtt.client as mqtt
 from systemd.daemon import notify, Notification
+import vcgencmd
 
 
 DEFAULTS = {
     'logLevel': "INFO",  #"DEBUG"  #"WARNING"
-    'watchdog': 15  # watchdog interval in usecs (15 secs)
+    'watchdog': 15,      # watchdog interval in usecs (15 secs),
+    'mqttBroker': "gpuServer1"
 }
+
+MQTT_TOPIC = "/sensors/avue"
+APPL_NAME = "AvuePZT"
+APPL_VERSION = "1.1.0"
+DEV_TYPE = "Rpi3"
+
+
+vcgen = None
+
+
+#### TODO put this in a common file
+def macAddress(hostname):
+    """Return the MAC address and interface name used to reach the given host
+
+      Returns usable strings on error.
+
+      Inputs:
+        hostname: string name of the target host
+      Returns: string name and string form of MAC address of interface used to
+       reach the given host
+    """
+    ipAddr = socket.gethostbyname(hostname)
+    match = re.search(r"^.* dev (.*?) .*$",
+                      str(subprocess.check_output(["ip", "route", "get", ipAddr])))
+    if not match:
+        logging.warning(f"Unable to find interface for {ipAddr}")
+        return "n/a", "00:00:00:00:00:00"
+    intf = match.group(1)
+    try:
+        macAddr = subprocess.check_output(["cat", f"/sys/class/net/{intf}/address"]).strip().decode("utf-8")
+    except Exception as ex:
+        logging.warning("Failed to get MAC address for '{hostname}': {ex}")
+        return "n/a", "00:00:00:00:00:00"
+    return intf, macAddr
+
+
+#### TODO put this in a common file
+def wifiQuality(interface):
+    """Return a quality indicator and RSSI of the given WiFi interface
+
+      Returns usable strings on error.
+
+      Inputs:
+        interface: string name of WiFi interface for which to get signal quality
+
+      Returns: float value from 0.0 to 1.0 indicating worst to best signal
+       quality, respectively, and the RSSI value in dBm
+    """
+    match = re.search(r'.* Quality=([0-9]+)/([0-9]+)  Signal level=(.*) dBm .*$',
+                      str(subprocess.check_output(["iwlist", interface, "scan"])))
+    if not match:
+        logging.warning(f"Unable to scan interface '{interface}'")
+        return "-0.0", "0"
+    vals = match.groups()
+    if len(vals) != 3:
+        logging.warning(f"Unable to get WiFi quality: {match.groups()}")
+        return "-0.0", "0"
+    return int(vals[0])/int(vals[1]), vals[2]
+
+
+#### TODO put this in a common file
+def deviceTemperature():
+    """Return the temperature of the device in degrees C
+
+      Returns: float value of device temperature in degrees C
+    """
+    if not vcgen:
+        vcgen = vcgencmd.Vcgencmd()
+    return vcgen.measure_temp()
 
 
 #### TODO put this in a common file
@@ -65,20 +138,36 @@ class Watchdog():
 
 
 def run(options):
-    '''
+    running = True
+
+    def shutdownHandler(signum, frame):
+        logging.debug(f"Caught signal: {signum}")
+        running = False
+
     for s in ('TERM', 'HUP', 'INT'):
         sig = getattr(signal, 'SIG'+s)
         signal.signal(sig, shutdownHandler)
-    '''
-    #### TODO put work here
-    time.sleep(10)
+
+    intfName, macAddr = macAddress(options.mqttBroker)
+    TOPIC_BASE = f"{MQTT_TOPIC}/{macAddr}/cmd/"
+    quality, rssi = wifiQuality(intfName)
+
+    logging.info("Starting")
+    #### FIXME
+    print(f"{TOPIC_BASE},Startup,{DEV_TYPE},{APPL_NAME},{APPL_VERSION},temp:.1f,q:.4f,rssi:d,{rssi}")
+    while running:
+        temperature = deviceTemperature()
+        quality, rssi = wifiQuality()
+        #### FIXME
+        print(f"{TOPIC_BASE}/data,{temperature},{quality:.4f},{rssi}")
+        time.sleep(options.watchdog)
     logging.info("Exiting")
     return(0)
 
 
 def getOpts():
     usage = f"Usage: {sys.argv[0]} [-v] [-L <logLevel>] [-l <logFile>] " + \
-      "[-w <watchdog>]"
+      "[-m <mqttHost>] [-w <watchdog>]"
     ap = argparse.ArgumentParser()
     ap.add_argument(
         "-L", "--logLevel", action="store", type=str,
@@ -88,6 +177,10 @@ def getOpts():
     ap.add_argument(
         "-l", "--logFile", action="store", type=str,
         help="Path to location of logfile (create it if it doesn't exist)")
+    ap.add_argument(
+        "-m", "--mqttBroker", action="store", type=str,
+        default=DEFAULTS['mqttBroker'],
+        help="Hostname for where the MQTT broker is running")
     ap.add_argument(
         "-v", "--verbose", action="count", default=0,
         help="Enable printing of debug info")
@@ -109,6 +202,7 @@ def getOpts():
     opts.watchdog = 0 if opts.watchdog < 1 else opts.watchdog
 
     if opts.verbose:
+        print(f"    MQTT Broker:        {opts.mqttBroker}")
         if opts.watchdog:
             print(f"    Watchdog Interval:  {opts.watchdog} secs")
         else:
