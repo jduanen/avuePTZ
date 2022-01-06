7ccb99a19445f9e3f6d7579dbc8c88880c201d80
Duane Northcutt <duane.northcutt@gmail.com>
2022-01-05 16:53:30 -0800

    Created.

---
+++ avueNightVideo.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Script that runs ffmpeg to serve the video stream and issues watchdog updates
+#
+# Setup by installing 'avueVideo.service' in /lib/systemd/sysetm and running:
+#  sudo systemctl daemon-reload
+#  sudo systemctl start avueVideo
+#  sudo systemctl status avueVideo
+#
+# To view video: 'vlc http://avue:8554'
+
+
+INTERVAL=15
+WATCHDOG=false
+
+function cleanup()
+{
+	kill $PID
+    /bin/systemd-notify --pid $PID --status=STOPPING
+	exit
+}
+
+trap cleanup EXIT
+
+ffmpeg tmix=frames=7:weights="1 1 1 1 1 1 1" -fflags +genpts+igndts -i /dev/video0 -c:v h264_omx -an -b:v 8M -f mpegts - | cvlc -I dummy - --sout='#std{access=http,mux=ts,dst=:8554,acodec=none}' & PID=$!
+
+/bin/systemd-notify --pid $PID --ready
+while true; do
+	if ps -p $PID > /dev/null; then
+		if $WATCHDOG; then
+			/bin/systemd-notify --pid $PID WATCHDOG=1
+		fi
+	else
+		echo "PID: " $PID " not running"
+		break
+	fi
+	sleep $INTERVAL
+done
+
+kill $PID
